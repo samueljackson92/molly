fd48e4badb455131eaf65bc2aba626df012bc936
Samuel Jackson <samueljackson@outlook.com>
2016-03-10 22:29:57 +0000

    Add property measurements
    Also fix issue with integration in the wrong order!!

---
+++ Property.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Samuel Jackson on 10/03/2016.
+//
+
+#include "Property.h"
---
+++ Property.h
@@ -0,0 +1,14 @@
+//
+// Created by Samuel Jackson on 10/03/2016.
+//
+
+#ifndef MOLLY_PROPERTY_H
+#define MOLLY_PROPERTY_H
+
+
+class Property {
+
+};
+
+
+#endif //MOLLY_PROPERTY_H
